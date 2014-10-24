06773cf3ef7937bc1b57de07fe11af8a0df97296
atlas2ninjas <atlas2ninjas@lonelyplanet.com.au>
2014-10-24 16:55:18 +1100

    Add inspector to blueprint. It returns line for an easy debugging on vigia

--- lib/vigia/blueprint.rb
+++ lib/vigia/blueprint.rb
@@ -3,12 +3,40 @@
 module Vigia
   class Blueprint
 
-    attr_reader :apib, :metadata
+    attr_reader :apib, :apib_source, :metadata
 
     def initialize apib_source
+      @apib_source = apib_source
       @apib_parsed = RedSnow::parse(apib_source)
       @apib        = @apib_parsed.ast
       @metadata    = @apib.metadata
+    end
+
+    def inspector object
+      case object
+      when RedSnow::Blueprint
+        { line: 0 }
+      when RedSnow::ResourceGroup
+        locate_in_source("# Group #{ object.name }")
+      when RedSnow::Resource
+        locate_in_source(object.uri_template)
+      when RedSnow::Action
+        locate_in_source("#{ object.name } [#{ object.method }]")
+      else
+        { line: 'undefined' }
+      end
+    end
+
+    private
+
+    def locate_in_source(text)
+      match = {}
+      apib_source.lines.each_with_index do |line, index|
+        match = { line: index + 1 }  if line.strip.include?(text)
+
+        break unless match.empty?
+      end
+      match
     end
   end
 end
--- spec/lib/blueprint_spec.rb
+++ spec/lib/blueprint_spec.rb
@@ -11,7 +11,6 @@ describe Vigia::Blueprint do
   end
 
   describe '#initialize' do
-
     it 'calls RedSnow::parse' do
       subject
       expect(RedSnow).to have_received(:parse).with(Vigia::ExampleTest.apib_source)
@@ -23,6 +22,74 @@ describe Vigia::Blueprint do
 
     it 'has a #apib getter' do
       expect(subject.apib).to eql(Vigia::ExampleTest.apib.ast)
+    end
+  end
+
+  describe '#inspector' do
+    let(:inspector) { subject.inspector(object) }
+
+    context 'when inspecting the Api Blueprint' do
+      let(:object) { subject.apib }
+
+      it 'returns the right data for a resource group' do
+        expect(inspector).to eql(line: 0)
+      end
+    end
+
+    context 'when inspecting a Resource Group' do
+      let(:object) { subject.apib.resource_groups.first }
+
+      it 'returns the right data for a resource group' do
+        expect(inspector).to eql(line: 7)
+      end
+    end
+
+    context 'when inspecting a the last resource group' do
+      let(:object) { subject.apib.resource_groups.last }
+
+      it 'returns the right data for a resource group' do
+        expect(inspector).to eql(line: 161)
+      end
+    end
+
+    context 'when inspecting a the first resource' do
+      let(:object) { subject.apib.resource_groups.first.resources.first }
+
+      it 'returns the right data for a resource' do
+        expect(inspector).to eql(line: 9)
+      end
+    end
+
+    context 'when inspecting a the last resource ' do
+      let(:object) { subject.apib.resource_groups.last.resources.last }
+
+      it 'returns the right data for a resource' do
+        expect(inspector).to eql(line: 233)
+      end
+    end
+
+    context 'when inspecting a the first action' do
+      let(:object) { subject.apib.resource_groups.first.resources.first.actions.first }
+
+      it 'returns the right data for the action' do
+        expect(inspector).to eql(line: 12)
+      end
+    end
+
+    context 'when inspecting a the last action ' do
+      let(:object) { subject.apib.resource_groups.last.resources.last.actions.last }
+
+      it 'returns the right data for the action' do
+        expect(inspector).to eql(line: 270)
+      end
+    end
+
+    context 'when inspecting a not valid object' do
+      let(:object) { Array.new }
+
+      it 'returns the right data for an unknown object ' do
+        expect(inspector).to eql(line: 'undefined')
+      end
     end
   end
 end
